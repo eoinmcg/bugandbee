1603a491acf1eb2bb29682e4bbdfe5f9cc9977ee
Eoin McGrath <eoin.mcg@gmail.com>
2025-11-26 13:58:49 +0200

    posts score to newgrounds

--- src/helpers/postScore.js
+++ src/helpers/postScore.js
@@ -1,11 +1,11 @@
 import keys from "../data/keys";
 
 export default function postScore(score, game) {
-  console.log({
-    message: 'post score',
-    score: score,
-    isNewgrounds: game.isNewgrounds
-  });
-  // game.ng.postScore(keys.ScoreBoard, score);
+  // console.log({
+  //   message: 'post score',
+  //   score: score,
+  //   isNewgrounds: game.isNewgrounds
+  // });
+  game.ng.postScore(keys.ScoreBoard, score);
 }
 
