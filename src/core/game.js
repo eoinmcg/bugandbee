ec8a4507eec2a2c6114bd533198b1173956b7390
Eoin McGrath <eoin.mcg@gmail.com>
2025-12-08 18:43:30 +0200

    increases mobile joystick size

--- src/core/game.js
+++ src/core/game.js
@@ -118,7 +118,7 @@ Game.sceneManager = sceneManager;
 tileFixBleedScale = .1;
 setTouchGamepadEnable(true);
 setTouchGamepadButtonCount(1);
-setTouchGamepadSize(160);
+setTouchGamepadSize(180);
 setCanvasClearColor(new Color(0, 0, 0, 0));
 
 if (window.BUILD) {
