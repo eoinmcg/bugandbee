25cf5e798823154188cca7aa5c35a48f28fef3c9
Eoin McGrath <eoin.mcg@gmail.com>
2026-01-28 13:36:20 +0200

    updates to littlejs 1.17.15

--- src/scenes/select.js
+++ src/scenes/select.js
@@ -1,13 +1,11 @@
 import Scene from "./scene";
-// import palette from "../data/palette";
 
 export default class Select extends Scene {
-
   enter(Game) {
     this.g = Game;
 
     this.change = false;
-    this.bgColor = new Color(.1, .1, .1)
+    this.bgColor = new Color(0.1, 0.1, 0.1);
 
     this.active = 0;
     this.stick = [gamepadDpad(0)];
@@ -16,48 +14,51 @@ export default class Select extends Scene {
     this.frameCount = 0;
     this.frame = 0;
 
-    this.initTunnel('emerald', 'dark_teal', 0, .09);
-
+    this.initTunnel("emerald", "dark_teal", 0, 0.09);
   }
 
   update() {
-
     const stick = gamepadStick(0);
 
-    this.frameCount += (timeDelta * 9);
+    this.frameCount += timeDelta * 9;
     if (this.frameCount > 1) {
       this.frameCount = 0;
-      this.frame = (this.frame >= 2)
-        ? 0 : this.frame += 1;
+      this.frame = this.frame >= 2 ? 0 : (this.frame += 1);
     }
 
-    if (keyWasPressed('ArrowLeft') || (this.lastStick[0] < 0 && stick.x === 0)) {
+    if (
+      keyWasPressed("ArrowLeft") ||
+      (this.lastStick[0] < 0 && stick.x === 0)
+    ) {
       this.active -= 1;
-      this.g.sfx.play('walk');
+      this.g.sfx.play("walk");
     }
-    if (keyWasPressed('ArrowRight') || (this.lastStick[0] > 0 && stick.x === 0)) {
+    if (
+      keyWasPressed("ArrowRight") ||
+      (this.lastStick[0] > 0 && stick.x === 0)
+    ) {
       this.active += 1;
-      this.g.sfx.play('walk');
+      this.g.sfx.play("walk");
     }
 
     if (this.active < 0) this.active = 1;
     if (this.active > 1) this.active = 0;
 
-    if (keyWasPressed('Enter')
-      || keyWasPressed('KeyX')
-      || gamepadWasPressed(0)
-      || gamepadWasPressed(1)
-      || gamepadWasPressed(2)
-      || keyWasPressed('Space')) {
-      this.g.sfx.play('alert');
-      this.g.store.p1.type = this.active === 1
-        ? 'BEE' : 'BUG';
-      const scene = this.g.plays === 0 ? 'Tutorial' : 'Play';
+    if (
+      keyWasPressed("Enter") ||
+      keyWasPressed("KeyX") ||
+      gamepadWasPressed(0) ||
+      gamepadWasPressed(1) ||
+      gamepadWasPressed(2) ||
+      keyWasPressed("Space")
+    ) {
+      this.g.sfx.play("alert");
+      this.g.store.p1.type = this.active === 1 ? "BEE" : "BUG";
+      const scene = this.g.plays === 0 ? "Tutorial" : "Play";
       this.g.sceneManager.changeScene(scene);
     }
 
     this.lastStick = [stick.x];
-
   }
 
   renderPost() {
@@ -68,26 +69,37 @@ export default class Select extends Scene {
     drawRect(vec2(0, 0), vec2(w, h), new Color(0, 0, 0, 0.9));
 
     this.logoText({
-      text: 'SELECT', pos: vec2(0, 10), size: 3, color: WHITE,
-      lineColor: BLACK
+      text: "SELECT",
+      pos: vec2(0, 10),
+      size: 3,
+      color: WHITE,
+      lineColor: BLACK,
     });
 
     drawRect(vec2(-5, 0), vec2(8.4), this.active === 0 ? RED : BLACK);
     drawRect(vec2(-5, 0), vec2(8), this.bgColor);
-    const bugFrame = this.active === 0
-      ? `bug${this.frame}` : 'bug3';
+    const bugFrame = this.active === 0 ? `bug${this.frame}` : "bug3";
     drawTile(vec2(-5, 0), vec2(4), this.g.tile(bugFrame));
-    let col = this.active === 0 ? 'red' : 'gray';
-    font.drawText('BUG', vec2(-5, -6), this.active === 0 ? 1.2 : 1, true, this.g.palette[col].mk());
+    let col = this.active === 0 ? "red" : "gray";
+    font.drawText(
+      "BUG",
+      vec2(-5, -6),
+      this.active === 0 ? 1.2 : 1,
+      true,
+      this.g.palette[col].mk(),
+    );
 
     drawRect(vec2(5, 0), vec2(8.4), this.active === 1 ? YELLOW : BLACK);
     drawRect(vec2(5, 0), vec2(8), this.bgColor);
-    const beeFrame = this.active === 1
-      ? `bee${this.frame}` : 'bee3';
+    const beeFrame = this.active === 1 ? `bee${this.frame}` : "bee3";
     drawTile(vec2(5, -1), vec2(4), this.g.tile(beeFrame));
-    col = this.active === 1 ? 'yellow' : 'gray';
-    font.drawText('BEE', vec2(5, -6), this.active === 1 ? 1.2 : 1, true, this.g.palette[col].mk());
+    col = this.active === 1 ? "yellow" : "gray";
+    font.drawText(
+      "BEE",
+      vec2(5, -6),
+      this.active === 1 ? 1.2 : 1,
+      true,
+      this.g.palette[col].mk(),
+    );
   }
-
 }
-
--- src/scenes/tutorial.js
+++ src/scenes/tutorial.js
@@ -1,24 +1,24 @@
-import { outlineTile } from '../helpers/drawOutline';
-import Powerup from '../sprites/powerup';
-import Scene from './scene';
+import { outlineTile } from "../helpers/drawOutline";
+import Powerup from "../sprites/powerup";
+import Scene from "./scene";
 
 export default class Tutorial extends Scene {
   enter(Game) {
     super.enter(Game);
     this.g = Game;
 
-    this.initTunnel('maroon', 'red', 0.05, .07);
-    this.enemies = ['worm0', 'skull', 'eye', 'bat0', 'drone0', 'creep0']
+    this.initTunnel("maroon", "red", 0.05, 0.07);
+    this.enemies = ["worm0", "skull", "eye", "bat0", "drone0", "creep0"];
     this.changeEnemy = 0;
     this.enemySprite = this.enemies.rnd();
 
-    this.sfxs = ['smash', 'key', 'jet', 'score'];
+    this.sfxs = ["smash", "key", "jet", "score"];
 
     this.step = 0;
-    this.title = 'Tutorial';
-    this.titleTyped = '';
+    this.title = "Tutorial";
+    this.titleTyped = "";
     this.titleTimer = new Timer();
-    this.typeSpeed = .1;
+    this.typeSpeed = 0.1;
     this.titleTimer.set(this.typeSpeed);
 
     this.timer = new Timer();
@@ -26,85 +26,89 @@ export default class Tutorial extends Scene {
     this.timer.set(this.interval);
 
     this.powerup = new Powerup(this.g, vec2(-11, -20), false);
-
   }
 
   update() {
-
     if (this.titleTimer.elapsed() && this.titleTyped !== this.title) {
       this.titleTyped += this.title.charAt(this.titleTyped.length);
-      this.g.sfx.play('score');
+      this.g.sfx.play("score");
       this.titleTimer.set(this.typeSpeed);
     }
 
     if (this.timer.elapsed() && this.step < 4) {
-
       this.g.sfx.play(this.sfxs[this.step]);
       this.timer.set(this.interval);
       this.step += 1;
     }
 
-
-    this.changeEnemy += timeDelta * .5;
+    this.changeEnemy += timeDelta * 0.5;
     if (this.changeEnemy > 1) {
       this.changeEnemy = 0;
       this.enemySprite = this.enemies.rnd();
     }
     this.charge = this.changeEnemy * 100;
 
-    if (keyWasPressed('Enter')
-      || keyWasPressed('KeyX')
-      || gamepadWasPressed(0)
-      || gamepadWasPressed(1)
-      || gamepadWasPressed(2)
-      || keyWasPressed('Space')) {
+    if (
+      keyWasPressed("Enter") ||
+      keyWasPressed("KeyX") ||
+      gamepadWasPressed(0) ||
+      gamepadWasPressed(1) ||
+      gamepadWasPressed(2) ||
+      keyWasPressed("Space")
+    ) {
       if (this.step < 3) return;
-      this.g.sceneManager.changeScene('Play');
+      this.g.sceneManager.changeScene("Play");
     }
   }
 
   renderPost() {
-
-    this.renderTint(.9);
+    this.renderTint(0.9);
 
     const wave = Math.sin(new Date().getTime() * 0.009);
     const font = engineFontImage;
 
     setFontDefault('"04b_19"');
-    drawText(this.titleTyped, vec2(-3, 11), 2, WHITE, 0, WHITE, 'left');
+    drawText(this.titleTyped, vec2(-3, 10), 2, WHITE, 0, WHITE, "left");
 
     let startX = -11;
 
     if (this.step > 0) {
-      outlineTile({ pos: vec2(startX, 5), size: vec2(1.2), tileInfo: this.g.tile(this.enemySprite) });
-      font.drawText('Shoot baddies', vec2(startX + 2, 5), .9, false);
+      outlineTile({
+        pos: vec2(startX, 5),
+        size: vec2(1.2),
+        tileInfo: this.g.tile(this.enemySprite),
+      });
+      font.drawText("Shoot baddies", vec2(startX + 2, 5), 0.9, false);
     }
 
     if (this.step > 1) {
       this.powerup.pos = vec2(-11, 2);
       this.powerup.render();
 
-      font.drawText('Collect powerups', vec2(startX + 2, 2), .9, false);
-
+      font.drawText("Collect powerups", vec2(startX + 2, 2), 0.9, false);
     }
 
     if (this.step > 2) {
       outlineTile({
         pos: vec2(startX, -1),
-        size: vec2(2 * this.charge / 100),
-        tileInfo: this.g.tile('circle'),
-        color: this.charge > 80 ? WHITE : new Color(1, .7, 0, this.charge / 100),
+        size: vec2((2 * this.charge) / 100),
+        tileInfo: this.g.tile("circle"),
+        color:
+          this.charge > 80 ? WHITE : new Color(1, 0.7, 0, this.charge / 100),
         angle: -time,
         outlineColor: RED,
-        outlineOffset: .2
+        outlineOffset: 0.2,
       });
-      font.drawText('Hold fire for MegaShot', vec2(startX + 2, -1), .9, false);
+      font.drawText("Hold fire for MegaShot", vec2(startX + 2, -1), 0.9, false);
     }
 
     if (this.step > 3 && wave > 0) {
-      font.drawText('Now go kick some butt!', vec2(2, -10), .9, this.g.palette.pink.mk());
+      font.drawText(
+        "Now go kick some butt!",
+        vec2(2, -10),
+        0.9,
+        this.g.palette.pink.mk(),
+      );
     }
-
   }
-
 }
